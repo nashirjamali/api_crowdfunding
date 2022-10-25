3b0098b2bd4f2855a803d2ce0ee0af65129b5bbd
Nashir Jamali <nashirjamali@gmail.com>
2022-10-25 17:45:18 +0700

    WIP campign

--- campaign/repository.go
+++ campaign/repository.go
@@ -1,1 +1,20 @@
 package campaign
+
+import "gorm.io/gorm"
+
+type Repository interface {
+	FindAll() ([]Campaign, error)
+	FindByUserID(userID int) ([]Campaign, error)
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) *repository {
+	return &repository{db}
+}
+
+func FindAll() ([]Campaign, error) {
+	
+}
